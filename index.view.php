aeb95e7d08ec90bffc125d50b09fa9ca50d4e51b
Xerna-PC-Linux <ricardo.cerna2001@gmail.com>
2022-01-02 20:53:48 -0600

    listo

--- index.view.php
+++ index.view.php
@@ -9,20 +9,25 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post" class="formulario">
-        <input type="text" name ="nombre" placeholder="Escribe tu nombre" value="">
-        <input type="email" name="correo" placeholder="Escribe tu correo" value="">
-        <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje"></textarea>
-            <?php if (!empty($errores)): ?>
-				<div class="alert error">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="formulario">
+			
+			<input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre:" value="<?php if(!$enviado && isset($nombre)) echo $nombre?>">
+		
+			<input type="text" class="form-control" name="correo" id="correo" placeholder="Correo:" value="<?php if(!$enviado && isset($correo)) echo $correo?>">
+		
+			<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"><?php if(!$enviado && isset($mensaje)) echo $mensaje?></textarea>
+		
+			<?php if (!empty($errores)): ?>
+				<div class="alert error" role="alert">
 					<?php echo $errores; ?>
 				</div>
 			<?php elseif($enviado) : ?>
-				<div class="alert success">
+				<div class="alert succes" role="alert">
 					<?php echo 'Enviado Correctamente'; ?>
 				</div>
-			<?php endif ?>
-        <input type="submit" name ="submit" value="Enviar" class="btn-primary">
-    </form>
+			<?php endif; ?>
+		
+			<input type="submit" name="submit" class="btn-primary" value="Enviar Correo">
+		</form>
 </body>
 </html>
